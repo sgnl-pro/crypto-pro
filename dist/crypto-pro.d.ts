22d1a46073f452f7e9850073ab2cc98c611efd5a
Барциц Вячеслав <bartsitsvv@it2g.ru>
2021-06-24 18:29:36 +0300

    @sgnl-pro/crypto-pro 2.2.3

--- dist/crypto-pro.d.ts
+++ dist/crypto-pro.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for @sgnl-pro/crypto-pro-tsa 2.2.2
+// Type definitions for @sgnl-pro/crypto-pro-tsa 2.2.3
 // Project: @sgnl-pro/crypto-pro-tsa
 // Definitions by: Vitalii Goma https://github.com/vgoma
 
